5f4273ff02b8b816eae3ebc3c725d5a0452c3e97
Logan Saunders <lsaunders275@gmail.com>
2024-04-01 21:18:14 -0600

    initial commit

---
+++ src/lib/ButtonLink.svelte
@@ -0,0 +1,6 @@
+<script lang='ts'>
+  export let href: string = ''
+  export let target: string = '_blank'
+</script>
+
+<a {href} {target} class='px-6 py-3 bg-orange-100 rounded-md text-orange-900'><slot /></a>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import {ButtonLink} from "$lib";
+</script>
+
+<section class='grid place-content-center h-full bg-gradient-to-br from-amber-600 to-yellow-400'>
+  <div class='flex flex-col items-center text-orange-100 font-bold text-5xl'>
+    <h1>SvelteKit</h1>
+    <h1>+</h1>
+    <h1>Tailwind</h1>
+  </div>
+
+  <div class='mb-24'/>
+  <p class='text-orange-100 max-w-96 text-center'>Visit the documentation for more information.</p>
+
+  <div class='flex justify-center gap-6 mt-4'>
+    <ButtonLink href="https://kit.svelte.dev/docs">SvelteKit</ButtonLink>
+    <ButtonLink href="https://tailwindcss.com/docs">Tailwind</ButtonLink>
+  </div>
+</section>
